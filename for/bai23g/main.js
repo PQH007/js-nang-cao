7b8b90ff2709379b1b788d2b5abdd56a263f179a
PQHtobi <phanquochung2511@gmail.com>
2022-08-26 11:54:04 +0700

    .

--- for/bai23g/main.js
+++ for/bai23g/main.js
@@ -1,13 +1,16 @@
 document.getElementById('btn').onclick = function(){
     var n = parseInt(document.getElementById('n').value);
     var k = 0;
-        for(var i = 1; i <= n; ++i,k = 0){
-            for(var j = i; j <=n ; j++){
+        for(var i = n; i >= 1; i--){
+            for(var j = i; j <= n ; j++){
                 document.write('&nbsp;&nbsp;');
+                console.log(j)
             }
-            while (k != (2 * i) - 1) {
+            for(var k = i;k <=(2*i)-1;k++) {
                 document.write("* ");
-                ++k;
+             }
+             for(var k = 0; k < i -1; k++){
+                document.write("* ");
              }
             document.write('<br />');
          }
