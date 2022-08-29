a145b042a6caf89180b7156101b69a83d8236dd1
PQHtobi <phanquochung2511@gmail.com>
2022-08-29 23:44:36 +0700

    .

---
+++ function/bai2/main.js
@@ -0,0 +1,9 @@
+function bai2(a,b,c,sum){
+    for(i = 0; i <=a ; i++){
+        if(a % i == 0 && b % i ==0 && c % i ==0){
+            sum = i;
+        }
+    }
+    return sum;
+}
+console.log(bai2(60,20,12));
