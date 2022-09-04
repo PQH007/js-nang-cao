4e397a3a9bf9a19e07635816e481102e17c12afb
PQHtobi <phanquochung2511@gmail.com>
2022-09-04 13:45:21 +0700

    .

--- function/bai7/main.js
+++ function/bai7/main.js
@@ -1,11 +1,15 @@
-
-
-document.getElementById('btn').onclick = function tinhgiaithua(){
-    var n = document.getElementById('n').value;
-    for(var i = 1;i <= n;i++){
-        var giaithua = 1;
-         giaithua = giaithua * i;
-    }
-    console.log(giaithua);
+var sum = 0;
+document.getElementById('btn').onclick = function ketqua(){
+    tinhgiaithua();
+    document.write('ket qua = '+ sum);
 }
-
+function tinhgiaithua(){
+    var n = document.getElementById('n').value;
+    var giaithua = 1;
+    for(var i = 1;i <= n;i++){
+       if(i % 2 ==0 ){
+        giaithua = giaithua * i;
+       }
+       sum = sum + giaithua;
+    }
+}
